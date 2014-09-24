d03a721f12cc23433a61a34fa00addd950fbba72
Tim Vergenz <tvergenz@palantir.com>
2014-09-24 16:45:04 -0400

    Rewrite hub.js and add task dependencies.

--- tasks/hub.js
+++ tasks/hub.js
@@ -9,101 +9,150 @@
 module.exports = function(grunt) {
   'use strict';
 
-  var path = require('path');
-  var chalk = require('chalk');
-  var async = require('async');
   var _ = require('lodash');
+  var async = require('async');
+  var chalk = require('chalk');
+  var path = require('path');
+
+  var defaultDependencyFns = {
+    // Dependency function to depend on a project's bower dependencies.
+   'bower': function(gruntfile, allGruntfiles) {
+      var projectPath = path.dirname(gruntfile);
+      var bowerJson = require(path.join(projectPath, "bower.json"));
+      var bowerDependencies = _.extend(bowerJson.dependencies, bowerJson.devDependencies);
+      var dependencies = [];
+      // for each dependency...
+      Object.keys(bowerDependencies).forEach(function(dependencyName) {
+        var dependencyValue = bowerDependencies[dependencyName];
+        var dependencyPath = path.resolve(projectPath, dependencyValue);
+        // check if there's a Gruntfile we know about in that directory...
+        allGruntfiles.forEach(function(gruntfile2) {
+          if (path.dirname(gruntfile2) == dependencyPath) {
+            // and depend on that Gruntfile if so.
+            dependencies.push(gruntfile2);
+          }
+        });
+      });
+      return dependencies;
+    }
+  }
 
   grunt.registerMultiTask('hub', 'Run multiple grunt projects', function() {
-    var options = this.options({
-      concurrent: 3,
-      allowSelf: false
-    });
-    var args = (this.args.length < 1) ? false : this.args;
 
-    var done = this.async();
-    var errorCount = 0;
-    // Get process.argv options without grunt.cli.tasks to pass to child processes
-    var cliArgs = _.without.apply(null, [[].slice.call(process.argv, 2)].concat(grunt.cli.tasks));
-    // Get it's own gruntfile
+    var gruntTaskArgs = this.args;
+    var options = this.options({
+      allowSelf: false,
+      dependencyFn: false
+    });
+
+    // Get hub's own Gruntfile.
     var ownGruntfile = grunt.option('gruntfile') || grunt.file.expand({filter: 'isFile'}, '{G,g}runtfile.{js,coffee}')[0];
     ownGruntfile = path.resolve(process.cwd(), ownGruntfile || '');
 
-    var lastGruntFileWritten;
-    function write(gruntfile, buf, isError) {
-      if (gruntfile !== lastGruntFileWritten) {
-        grunt.log.writeln('');
-        grunt.log.writeln('');
-        grunt.log.writeln(chalk.cyan('>> ') + gruntfile + ':\n');
-      }
-      grunt.log[(isError) ? 'error' : 'write'](buf);
-      lastGruntFileWritten = gruntfile;
-    }
+    var cliArgs = process.argv.slice(2)
+      .filter(function(arg, index, arr) {
+        return (
+          // Remove arguments that were tasks to this Gruntfile.
+          !(_.contains(grunt.cli.tasks, arg)) &&
+          // Remove "--gruntfile=project/Gruntfile.js" and "--gruntfile".
+          !(/^--gruntfile(=.*)?/.test(arg)) &&
+          // Remove anything that follows "--gruntfile" (i.e. as its argument).
+          !(index > 0 && arr[index-1] === '--gruntfile')
+        );
+      });
 
-    // our queue for concurrently ran tasks
-    var queue = async.queue(function(run, next) {
-      var skipNext = false;
-      grunt.log.ok('Running [' + run.tasks + '] on ' + run.gruntfile);
-      if (cliArgs) {
-        cliArgs = cliArgs.filter(function(currentValue) {
-          if (skipNext) return (skipNext = false);
-          var out = /^--gruntfile(=?)/.exec(currentValue);
-          if (out) {
-            if (out[1] !== '=') skipNext = true;
-            return false;
-          }
-          return true;
+    var errorCount = 0;
+    var asyncTasks = {};
+
+    // Create the async task callbacks and dependencies.
+    // See https://github.com/caolan/async#auto.
+    this.files.forEach(function(filesMapping) {
+
+      var gruntfiles = grunt.file.expand({filter: 'isFile'}, filesMapping.src)
+        .map(function(gruntfile) {
+          return path.resolve(gruntfile);
         });
+      if (!options.allowSelf) {
+        gruntfiles = _.without(gruntfiles, ownGruntfile);
       }
-      var child = grunt.util.spawn({
-        // Use grunt to run the tasks
-        grunt: true,
-        // Run from dirname of gruntfile
-        opts: {cwd: path.dirname(run.gruntfile)},
-        // Run task to be run and any cli options
-        args: run.tasks.concat(cliArgs || [], '--gruntfile=' + run.gruntfile)
-      }, function(err, res, code) {
-        if (err) { errorCount++; }
-        next();
-      });
-      child.stdout.on('data', function(buf) {
-        write(run.gruntfile, buf);
-      });
-      child.stderr.on('data', function(buf) {
-        write(run.gruntfile, buf, true);
-      });
-    }, options.concurrent);
-
-    // When the queue is all done
-    queue.drain = function() {
-      done((errorCount === 0));
-    };
-
-    this.files.forEach(function(files) {
-      var gruntfiles = grunt.file.expand({filter: 'isFile'}, files.src);
-      // Display a warning if no files were matched
       if (!gruntfiles.length) {
-        grunt.log.warn('No Gruntfiles matched the file patterns: "' + files.orig.src.join(', ') + '"');
+        grunt.log.warn('No Gruntfiles matched the file patterns: "' + filesMapping.orig.src.join(', ') + '"');
+        return;
       }
+
       gruntfiles.forEach(function(gruntfile) {
-        gruntfile = path.resolve(process.cwd(), gruntfile);
 
-        // Skip it's own gruntfile. Prevents infinite loops.
-        if (!options.allowSelf && gruntfile === ownGruntfile) { return; }
+        // Get the dependencies for this Gruntfile.
+        var dependencies = [];
+        var dependencyFn = filesMapping.dependencyFn || options.dependencyFn;
+        if (dependencyFn) {
+          if (typeof dependencyFn === 'string') {
+            dependencyFn = defaultDependencyFns[dependencyFn];
+          }
+          try {
+            dependencies = dependencyFn(gruntfile, gruntfiles);
+          }
+          catch (e) {
+            grunt.log.error(
+              'Could not get dependencies for Gruntfile (' + e.message + '): ' + gruntfile + '. ' +
+              'Assuming no dependencies.');
+          }
+        }
 
-        queue.push({
-          gruntfile: gruntfile,
-          tasks: args || files.tasks || ['default']
-        });
+        // Get the subtasks to run.
+        var gruntTasksToRun = (
+          (gruntTaskArgs.length < 1 ? false : gruntTaskArgs) ||
+          filesMapping.tasks ||
+          ['default']
+        );
+
+        // Create the async task function to run once all dependencies have run.
+        // Output is collected and printed as a batch once the task completes.
+        var asyncTaskFn = function(callback) {
+          var outputToWrite = [];
+          grunt.log.writeln('');
+          grunt.log.writeln(chalk.cyan('>> ') + 'Running [' + gruntTasksToRun + '] on ' + gruntfile);
+
+          // Spawn the child process.
+          var child = grunt.util.spawn({
+            grunt: true,
+            opts: {cwd: path.dirname(gruntfile)},
+            args: [].concat(gruntTasksToRun, cliArgs || [], '--gruntfile=' + gruntfile)
+          }, function(err, res, code) {
+            if (err) { errorCount++; }
+            grunt.log.writeln('');
+            grunt.log.writeln(chalk.cyan('>> ') + 'From ' + gruntfile + ':\n');
+            outputToWrite.forEach(function(lineData) {
+              lineData.fn(lineData.data);
+            });
+            callback(err);
+          });
+
+          // Buffer its stdout and stderr, to be printed on completion.
+          child.stdout.on('data', function(data) {
+            outputToWrite.push({
+              fn: grunt.log.write,
+              data: data
+            });
+          });
+          child.stderr.on('data', function(data) {
+            outputToWrite.push({
+              fn: grunt.log.error,
+              data: data
+            });
+          });
+        };
+
+        asyncTasks[gruntfile] = dependencies.concat([asyncTaskFn]);
       });
     });
 
-    //After processing all files and queueing them, make sure that at least one file is queued
-    if (queue.idle()) {
-        // If the queue is idle, assume nothing was queued and call done() immediately after sending warning
-        grunt.warn('No Gruntfiles matched any of the provided file patterns');
-        done();
-    }
+    var done = this.async();
+    async.auto(asyncTasks, function(err, results) {
+      grunt.log.writeln('');
+      grunt.log.writeln(chalk.cyan('>> ') + 'From ' + ownGruntfile + ':');
+      done(err);
+    });
 
   });
 
